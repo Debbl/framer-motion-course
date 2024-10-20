b35eb201809c9e1957937aa2b8906bd38ae5ca3d
Brendan Dash <me@aiwan.run>
2024-10-20 22:29:18 +0800

    feat: useAnimate

--- src/app/animation-controls/page.tsx
+++ src/app/animation-controls/page.tsx
@@ -1,32 +1,33 @@
 "use client";
-import { motion, useAnimationControls } from "framer-motion";
+import { useAnimate } from "framer-motion";
 
 export default function AnimationControls() {
-  const controls = useAnimationControls();
+  const [scope, animate] = useAnimate();
 
   const handleClick = async () => {
-    await controls.start("flip");
+    animate(scope.current, {
+      scale: 1.1,
+      rotate: "360deg",
+    });
+
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    await controls.start("initial");
+
+    animate(scope.current, {
+      scale: 1,
+      rotate: "0deg",
+    });
   };
 
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <button
-        onClick={handleClick}
+        onClick={() => handleClick()}
         className="my-2 rounded-md border px-2 py-1"
       >
         Flip it
       </button>
 
-      <motion.div
-        animate={controls}
-        variants={{
-          initial: { scale: 1, rotate: "0deg" },
-          flip: { scale: 1.1, rotate: "360deg" },
-        }}
-        className="size-24 rounded-md bg-blue-700"
-      ></motion.div>
+      <div ref={scope} className="size-24 rounded-md bg-blue-700"></div>
     </div>
   );
 }
